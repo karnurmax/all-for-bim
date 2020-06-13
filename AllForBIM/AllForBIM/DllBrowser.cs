fafd5b8eadf41bf6f54560e90860479aee77a9b9
karnurmax <karnurmax@mail.ru>
2020-06-13 15:10:50 +0500

    GetAllNamespaces GetAllTypesOfNamespace getAllMembersOfType

--- AllForBIM/AllForBIM/DllBrowser.cs
+++ AllForBIM/AllForBIM/DllBrowser.cs
@@ -19,13 +19,25 @@ namespace AllForBIM
             this.assembly = Assembly.LoadFile(this.revitAPIDllPath);
         }
 
-        internal IEnumerable<string> GetAllClasses()
+        internal IEnumerable<string> GetAllNamespaces()
         {
             var types = assembly.ExportedTypes;
 
             return types.Select(t => t.Namespace)
                 .Distinct();
 
+        }
+        internal IEnumerable<string> GetAllTypesOfNamespace(string nameSpace)
+        {
+            return assembly.ExportedTypes.Where(t => t.Namespace == nameSpace).Select(t => t.FullName);
+        }
+
+        internal IEnumerable<MemberInfo> getAllMembersOfType(string typeName)
+        {
+            var t = assembly.ExportedTypes.FirstOrDefault(c => c.FullName == typeName);
+            if (t == null)
+                return null;
+            return t.GetMembers();
         }
     }
 }
