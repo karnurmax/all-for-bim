21bc585a0625b67429f3ee7ad8ca184e7ad08b2e
karnurmax <karnurmax@mail.ru>
2020-06-13 13:22:15 +0500

    All for BIM console app begin

---
+++ AllForBIM/AllForBIM/DllBrowser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AllForBIM
+{
+    internal class DllBrowser
+    {
+        private readonly string revitAPIDllPath;
+        private readonly Assembly assembly;
+
+        public DllBrowser(string revitAPIDllPath)
+        {
+            this.revitAPIDllPath = revitAPIDllPath;
+            var bytes = File.ReadAllBytes(this.revitAPIDllPath);
+            this.assembly = Assembly.LoadFile(this.revitAPIDllPath);
+        }
+
+        internal IEnumerable<string> GetAllClasses()
+        {
+            var types = assembly.ExportedTypes;
+
+            return types.Select(t => t.Namespace)
+                .Distinct();
+
+        }
+    }
+}
---
+++ AllForBIM/AllForBIM/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AllForBIM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var revitAPIDllPath = @"C:\Program Files\Autodesk\Revit 2021\RevitAPI.dll";
+            var revitAPIUIDllPath = @"C:\Program Files\Autodesk\Revit 2021\RevitAPIUI.dll";
+            DllBrowser dllBrowser = new DllBrowser(revitAPIDllPath);
+            List<string> allClasses = dllBrowser.GetAllClasses().ToList();
+            allClasses.ForEach(cl => Console.WriteLine(cl));
+            Console.Write("Для выхода нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+    }
+}
